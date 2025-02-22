cf24dee0c1b1f54e7d22dd4a7385c7936a235528
azizbenmallouk <azizbenmallouk4@gmail.com>
2025-02-22 17:03:59 +0100

    looop

--- src/DataTable.jsx
+++ src/DataTable.jsx
@@ -1,16 +1,19 @@
 import TableRow from './TableRow';
 
 function DataTable(){
+    const tasks = ["Task 1", "Task 2", "Task 3", "Task 4"];
+    // const nbr = [1,2,3]; [2,4,6]
+    // nbr.map((i)=> i*2 )
+
     return (
         <table>
             <tr>
                 <th>Title</th>
                 <th>Action</th>
             </tr>
-            <TableRow data="test"/>
-            <TableRow data="test"/>
-            <TableRow data="test"/>
-            <TableRow data="test"/>
+            {
+                tasks.map((i)=> <TableRow data={i}/> )
+            }
         </table>
     );
 }

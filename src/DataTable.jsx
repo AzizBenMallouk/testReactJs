f31267049276ffcfbac017f9e195faa348e82e34
azizbenmallouk <azizbenmallouk4@gmail.com>
2025-02-22 16:52:44 +0100

    first commit

---
+++ src/DataTable.jsx
@@ -0,0 +1,17 @@
+function DataTable(){
+    return (
+        <table>
+            <tr>
+                <th>Title</th>
+                <th>Action</th>
+            </tr>
+            <tr>
+                <td>Title</td>
+                <td>Action</td>
+            </tr>
+        </table>
+    );
+}
+
+
+export default DataTable;
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import DataTable from './DataTable';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <DataTable/>
+);
